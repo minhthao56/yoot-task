7a696b8a6260dfbdce13a38a302c5b0f81e276c4
khanhhoa <truongkhanhhoa623@gmail.com>
2021-09-02 15:14:27 +0700

    sync

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,8 @@
-import { Button } from "../../components";
 import "./HomePage.scss";
 export const HomePage = () => {
   return (
-    <div>
-        <Button>BTN</Button>
+    <div className="home-page">
+		
     </div>
   );
 };
