43e8c8924fea880332f990241d365053935679a6
Nguyen Minh Thao <minhthao5648@gmail.com>
2021-09-02 18:39:50 +0700

    review-02/09/2021

--- src/components/Beadcrumb/Beadcrumb.tsx
+++ src/components/Beadcrumb/Beadcrumb.tsx
@@ -4,18 +4,16 @@ import "./Beadcrumb.scss";
 
 export const Beadcrumb: FC<IBeadcrumb> = ({ children, title }) => {
   return (
-    <>
-      <div className="beadcrumb">
-        <h3 className="beadcrumb__title">{title}</h3>
-        <ul className="links">
-          <li className="links__home">
-            <NavLink className="links__home--icon" to="/">
-              <i className="fas fa-home"></i>
-            </NavLink>
-          </li>
-          <li className="links__item">{title}</li>
-        </ul>
-      </div>
-    </>
+    <div className="beadcrumb">
+      <h3 className="beadcrumb__title">{title}</h3>
+      <ul className="links">
+        <li className="links__home">
+          <NavLink className="links__home--icon" to="/">
+            <i className="fas fa-home"></i>
+          </NavLink>
+        </li>
+        <li className="links__item">{title}</li>
+      </ul>
+    </div>
   );
 };
