c5f3a80cc32ccd49ece585ef7ac393f69c1f2d57
khanhhoa <truongkhanhhoa623@gmail.com>
2021-09-09 02:01:50 +0700

    test page/createVersion

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -4,12 +4,15 @@ import { PublicRouter } from "./PublicRouter";
 
 import { HomePage, LoginPage, VersionPage } from "../pages";
 import { MainLayout, BlankLayout } from "../layouts";
+import { CreateVersionPage } from "../pages/VersionPage/CreateVersionPage/CreateVersionPage";
 
 export const Routers = () => {
   return (
     <Switch>
       <PrivateRouter exact component={HomePage} layout={MainLayout} path="/" />
       <PrivateRouter exact component={VersionPage} layout={MainLayout} path="/version" />
+      <PrivateRouter exact component={CreateVersionPage} layout={MainLayout} path="/version/create" />
+
 
       <PublicRouter
         exact
