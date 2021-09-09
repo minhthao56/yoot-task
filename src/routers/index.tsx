01bf8770c5635f5289b6ab8d78a13ac936c2557d
khanhhoa <truongkhanhhoa623@gmail.com>
2021-09-09 18:20:49 +0700

    test pages

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -2,16 +2,19 @@ import { Switch } from "react-router-dom";
 import { PrivateRouter } from "./PrivateRouter";
 import { PublicRouter } from "./PublicRouter";
 
-import { HomePage, LoginPage, VersionPage } from "../pages";
+import { HomePage, LoginPage, PriorityPage, VersionPage } from "../pages";
 import { MainLayout, BlankLayout } from "../layouts";
-import { CreateVersionPage } from "../pages/VersionPage/CreateVersionPage/CreateVersionPage";
+import { CreatePriorityPage } from "../pages/CreatePriorityPage/CreatePriorityPage";
+import { CreateVersionPage } from "../pages/CreateVersionPage/CreateVersionPage";
 
 export const Routers = () => {
   return (
     <Switch>
       <PrivateRouter exact component={HomePage} layout={MainLayout} path="/" />
       <PrivateRouter exact component={VersionPage} layout={MainLayout} path="/version" />
+      <PrivateRouter exact component={PriorityPage} layout={MainLayout} path="/priority" />
       <PrivateRouter exact component={CreateVersionPage} layout={MainLayout} path="/version/create" />
+      <PrivateRouter exact component={CreatePriorityPage} layout={MainLayout} path="/priority/create" />
 
 
       <PublicRouter
